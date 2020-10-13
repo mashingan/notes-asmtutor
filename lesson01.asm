75b578a3af6852c04360b7c8a3b4cf3f052bbc5d
mashingan <rahmat.d.ruffy@gmail.com>
2020-10-13 19:11:21 +0700

    change hard coded length message

--- lesson01.asm
+++ lesson01.asm
@@ -4,7 +4,7 @@ entry start
 segment readable executable
 
 start:
-mov     edx, 13
+mov     edx, msglen
 mov     ecx, msg
 mov     ebx, 1
 mov     eax, 4
@@ -12,3 +12,4 @@ int     80h
 
 segment readable writeable
 msg     db 'Hello world!', 0Ah
+msglen = $ - msg
