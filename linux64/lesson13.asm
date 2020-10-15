ea2f26b1ec26c5346100500e1bc6f49ed50c0da7
mashingan <rahmat.d.ruffy@gmail.com>
2020-10-15 08:59:00 +0700

    linux64; lesson 13

---
+++ linux64/lesson13.asm
@@ -0,0 +1,11 @@
+format ELF64 executable 3
+entry start
+
+include 'procs.inc'
+
+segment readable executable
+start:
+    mov     eax, 90
+    sub     eax, 9
+    call    iprintLF
+    call    quitProgram
