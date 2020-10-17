b7d7a9560a3180596954bfc787a8d9db90f24555
mashingan <rahmat.d.ruffy@gmail.com>
2020-10-17 19:21:45 +0700

    remove unnecessary variable

--- win64/lesson01.asm
+++ win64/lesson01.asm
@@ -1,16 +1,14 @@
 format PE64 console
 entry start
-include 'd:/installer/fasmw/include/win64w.inc'
+include 'win64w.inc'
 
 section '.data' data readable writeable
 hello   db  'Hello world!', 0dh, 0ah, 0
 len = $ - hello
-conoutH dq  ?
 
 section '.text' code readable executable
 start:
     invoke  GetStdHandle, STD_OUTPUT_HANDLE
-    mov     [conoutH], rax
     push    0
     mov     rbx, rsp
     invoke  WriteConsoleA, rax, hello, len, rbx, 0
