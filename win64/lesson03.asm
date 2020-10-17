be55d05e152a1bf81f4aaa837b7a6c5b99795f17
mashingan <rahmat.d.ruffy@gmail.com>
2020-10-17 19:32:49 +0700

    win64; lesson 3

---
+++ win64/lesson03.asm
@@ -0,0 +1,28 @@
+format PE64 console
+entry start
+include 'win64w.inc'
+
+section '.data' readable
+msg     db 'Hello the new brave isekai!', 0dh, 0Ah, 0
+
+section '.text' code readable executable
+start:
+    mov     rbx, msg
+    mov     rcx, rbx
+
+.nextchar:
+    cmp     byte [rcx], 0
+    jz      .finished
+    inc     rcx
+    jmp     .nextchar
+
+.finished:
+    sub     rcx, rbx
+    invoke  GetStdHandle, STD_OUTPUT_HANDLE
+    invoke  WriteConsoleA, rax, msg, rcx, rbx, 0
+    invoke  ExitProcess, 0
+
+section '.idata' import data readable
+library kernel32, 'kernel32.dll'
+
+include 'api\kernel32.inc'
