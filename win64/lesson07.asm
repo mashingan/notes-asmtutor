6d1f499af16e180d02b63e015175758e47eab4a3
mashingan <rahmat.d.ruffy@gmail.com>
2020-10-17 22:25:40 +0700

    win64; lesson 7

---
+++ win64/lesson07.asm
@@ -0,0 +1,28 @@
+format PE64 console
+entry start
+
+include 'win64w.inc'
+
+section '.data' data readable writeable
+msg     db  'Hello the brave to the isekai!', 0
+msgf    db  'Adapt to the new environment with FASM.', 0
+stdout  dq ?
+
+section '.text' code readable executable
+
+start:
+    fastcall setStdout
+    mov     rcx, msg
+    fastcall sprintLF
+    
+    mov     rcx, msgf
+    fastcall sprintLF
+
+    fastcall quitProgram
+
+include 'procs.inc'
+
+section '.idata' import data readable
+library kernel32, 'kernel32.dll'
+
+include 'api\kernel32.inc'
