b2c6335ef0a914c72359445212a2dece38156070
mashingan <rahmat.d.ruffy@gmail.com>
2020-10-17 23:16:46 +0700

    win64; lesson 8

---
+++ win64/lesson08.asm
@@ -0,0 +1,24 @@
+format PE64 console
+entry start
+
+include 'win64w.inc'
+
+section '.data' data readable writeable
+stdout  dq ?
+cmdptr  dq ?
+
+section '.text' code readable executable
+
+start:
+    fastcall setStdout
+    invoke  GetCommandLineA
+    mov     rcx, rax
+    fastcall sprintLF
+    fastcall quitProgram
+
+include 'procs.inc'
+
+section '.idata' import data readable
+library kernel32, 'kernel32.dll'
+
+include 'api\kernel32.inc'
