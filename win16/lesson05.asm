0103057437c5e7e43efcf4c5c932e3445932617c
mashingan <rahmat.d.ruffy@gmail.com>
2020-10-21 06:53:57 +0700

    change sprint/snprint to print each character instead of string '$'

--- win16/lesson05.asm
+++ win16/lesson05.asm
@@ -5,9 +5,9 @@ start:
     call    sprint
     mov     dx, msgf
     call    sprint
-    int 20h
+    int     20h
 
 include 'procs.inc'
 
-msg     db  'Hello the brave to the isekai!', 0dh, 0Ah, 0, 0
-msgf    db  'Adapt to the new environment with FASM.', 0dh, 0Ah, 0, 0
+msg     db  'Hello the brave to the isekai!', 0dh, 0Ah, 0
+msgf    db  'Adapt to the new environment with FASM.', 0dh, 0Ah, 0
